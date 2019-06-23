d4b2a0ed40d8c54165043b6e1e9865d1d990ea07
GCCFeli <feli@gccfeli.cn>
2019-06-23 22:00:52 +0800

    refactor

--- ddpg.py
+++ ddpg.py
@@ -1,4 +1,4 @@
-def ddpg(n_episodes=2000, max_t=1000, print_every=30):
+def ddpg(n_episodes=500, max_t=1000, print_every=30):
     scores_deque = deque(maxlen=print_every)
     scores = []
     for i_episode in range(1, n_episodes+1):
@@ -9,8 +9,9 @@ def ddpg(n_episodes=2000, max_t=1000, print_every=30):
         for t in range(max_t):
             actions = agent.act(states)
             env_info = env.step(actions)[brain_name]
-            next_states, rewards, dones = env_info.vector_observations, env_info.rewards, env_info.local_done
-
+            next_states = env_info.vector_observations
+            rewards = env_info.rewards
+            dones = env_info.local_done
             agent.step(states, actions, rewards, next_states, dones)
             states = next_states
             score += rewards
@@ -18,14 +19,21 @@ def ddpg(n_episodes=2000, max_t=1000, print_every=30):
                 break
         scores_deque.append(np.mean(score))
         scores.append(np.mean(score))
-        print('\rEpisode {}\tScore: {:.2f}'.format(i_episode, np.mean(score)))
+        print('\rEpisode {}\t Score: {:.2f}'.format(i_episode, np.mean(score)))
+        if i_episode % print_every == 0:
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_deque)))
+        if np.mean(scores_deque) > 30.0:
+            print('Environment solved in {} episodes'.format(i_episode))
+            torch.save(agent.actor_local.state_dict(), 'checkpoint_actor.pth')
+            torch.save(agent.critic_local.state_dict(), 'checkpoint_critic.pth')
+            break
     return scores
 
 scores = ddpg()
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-plt.plot(np.arange(len(scores)), scores)
+plt.plot(np.arange(1, len(scores)+1), scores)
 plt.ylabel('Score')
 plt.xlabel('Episode #')
 plt.show()
