f39f98568574268e75a771510ec06d8ffc965599
GCCFeli <feli@gccfeli.cn>
2019-06-23 21:49:31 +0800

    refactor

--- ddpg.py
+++ ddpg.py
@@ -1,41 +1,31 @@
-def ddpg(n_episodes=2000, max_t=1000):
-    scores_deque = deque(maxlen=100)                          # last 100 scores
-    scores = []                                                # list containing scores from each episode
+def ddpg(n_episodes=2000, max_t=1000, print_every=30):
+    scores_deque = deque(maxlen=print_every)
+    scores = []
     for i_episode in range(1, n_episodes+1):
-        env_info = env.reset(train_mode=True)[brain_name]      # reset the environment
-        states = env_info.vector_observations                  # get the current state
+        env_info = env.reset(train_mode=True)[brain_name]
+        states = env_info.vector_observations
         agent.reset()
         score = np.zeros(num_agents)
         for t in range(max_t):
-            actions = agent.act(states)                        # select an action (for each agent)
-            env_info = env.step(actions)[brain_name]           # send all actions to tne environment
-            next_states, rewards, dones = env_info.vector_observations, env_info.rewards, env_info.local_done         # get next state (for each agent)
+            actions = agent.act(states)
+            env_info = env.step(actions)[brain_name]
+            next_states, rewards, dones = env_info.vector_observations, env_info.rewards, env_info.local_done
 
             agent.step(states, actions, rewards, next_states, dones)
-            states = next_states                               # roll over states to next time step
-            score += rewards                                   # update the score (for each agent)
-            if any(dones):                                     # exit loop if episode finished
+            states = next_states
+            score += rewards
+            if any(dones):
                 break
-        scores_deque.append(np.mean(score))                   # save most recent score
-        scores.append(np.mean(score))                          # save most recent score
+        scores_deque.append(np.mean(score))
+        scores.append(np.mean(score))
         print('\rEpisode {}\tScore: {:.2f}'.format(i_episode, np.mean(score)))
-        if i_episode % 100 == 0:
-            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_deque)))
-        if np.mean(scores_deque)>=30.0:
-            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_deque)))
-            torch.save(agents[0].actor_local.state_dict(), 'checkpoint_actor.pth')
-            torch.save(agents[0].critic_local.state_dict(), 'checkpoint_critic.pth')
-            break
     return scores
 
 scores = ddpg()
 
-# plot the scores
 fig = plt.figure()
 ax = fig.add_subplot(111)
 plt.plot(np.arange(len(scores)), scores)
 plt.ylabel('Score')
 plt.xlabel('Episode #')
 plt.show()
-
-env.close()
